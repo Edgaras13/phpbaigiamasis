c72171ca33aed564ce54559974be4c7afd9f2eb6
Edgaras <edgarasbytautas13@gmail.com>
2018-10-16 12:54:09 +0300

    HTML + PHP commit

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> As valdau HTML!</title>
+  </head>
+  <body>
+      
+      <h1><b><i>Vardas</i></b> - HTML asas/e</h1>
+      
+      <p>Moku kurti statini turini.</p>
+    
+  </body>
+</html>
+
 <?php
 print 'Veikia su GIT!';
